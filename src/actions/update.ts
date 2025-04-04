53d4442bc4effb78091cacadd0c60f576c91daab
brandoncintron <bcintron19fba@gmail.com>
2025-04-04 13:06:23 -0400

    Added update profile page and action

---
+++ src/actions/update.ts
@@ -0,0 +1,64 @@
+"use server";
+
+import * as z from "zod";
+
+import { db } from "@/lib/db";
+import { editProfileSchema } from "@/schema/profileSchema";
+import { auth } from "@/auth";
+
+export const updateProfile = async (
+  values: z.infer<typeof editProfileSchema>
+) => {
+  const validatedFields = editProfileSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields." };
+  }
+
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    return { error: "Unauthorized user." };
+  }
+
+  const {
+    name,
+    bio,
+    location,
+    pronouns,
+    preferredLanguage,
+    github,
+    linkedin,
+    website,
+    otherSocial,
+  } = validatedFields.data;
+
+  try {
+    await db.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        name,
+        bio,
+        location,
+        pronouns,
+        preferredLanguage,
+        github,
+        linkedin,
+        website,
+        otherSocial,
+      },
+    });
+
+    console.log("Server Action: Profile update successful for user:", {
+      name,
+      bio,
+    });
+    return { success: "Profile updated successfully!" };
+  } catch (error) {
+    console.error("Profile update failed:", error);
+    return { error: "Could not update your profile. Please try again." };
+  }
+};
