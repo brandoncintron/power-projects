cacfd8b3f6e27c83cbc21e2654a335ea48fff2a2
brandoncintron <bcintron19fba@gmail.com>
2025-04-27 14:32:36 -0400

    Added loading states to oauth buttons

--- src/components/auth/OAuthButtons.tsx
+++ src/components/auth/OAuthButtons.tsx
@@ -5,27 +5,54 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { signIn } from "next-auth/react"
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { useState } from "react";
+import { LoadingSpinner } from "@/components/ui/loading";
 
 /**
  * OAuthButtons - Sign in with Github and Google button
  */
 
 export default function OAuthButtons() {
-const onClick = (provider: "google" | "github") => {
+  const [isLoading, setIsLoading] = useState<"google" | "github" | null>(null);
+
+  const onClick = (provider: "google" | "github") => {
+    setIsLoading(provider);
     signIn(provider, {
-        callbackUrl: DEFAULT_LOGIN_REDIRECT,
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
     });
-}
-    
+  }
+
   return (
     <div className="flex flex-col space-y-2 w-full">
-        <Button onClick={() => onClick("github")} variant="outline" className="w-full">
-          <FaGithub className="mr-2 size-5" />
-          GitHub
+        <Button 
+          onClick={() => onClick("github")} 
+          variant="outline" 
+          className="w-full"
+          disabled={isLoading !== null}
+        >
+          {isLoading === "github" ? (
+            <LoadingSpinner className="h-5" text="Github" />
+          ) : (
+            <>
+              <FaGithub className="mr-2 size-5" />
+              GitHub
+            </>
+          )}
         </Button>
-        <Button onClick={() => onClick("google")} variant="outline" className="w-full">
-          <FcGoogle className="mr-2 size-5" />
-          Google
+        <Button 
+          onClick={() => onClick("google")} 
+          variant="outline" 
+          className="w-full"
+          disabled={isLoading !== null}
+        >
+          {isLoading === "google" ? (
+            <LoadingSpinner className="h-5" text="Google" />
+          ) : (
+            <>
+              <FcGoogle className="mr-2 size-5" />
+              Google
+            </>
+          )}
         </Button>
     </div>
   );
